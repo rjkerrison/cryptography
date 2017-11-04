cf15b8ca7c9e769aa45b6f740ef3e1f7d73ea8d4
rjkerrison <rjk1994@gmail.com>
2017-11-04 01:30:53 +0000

    React initialisation, and Caesar shift component

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to relllllooooad.
+        </p>
+        <Caesar/>
+      </div>
+    );
+  }
+}
+
+class Caesar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      encryptedValue: 'blah blah',
+      decryptedValue: 'akzi akzi',
+      shift: 1
+    }
+  }
+  render () {
+    return (
+      <div className="Caesar">
+        <input
+          name="shift"
+          type="number"
+          value={this.state.shift}
+          onChange={this.recalculate.bind(this)}/>
+        <textarea
+          name="encryptedValue"
+          type="text"
+          value={this.state.encryptedValue}
+          onChange={this.recalculate.bind(this)}></textarea>
+        <textarea
+          name="decryptedValue"
+          type="text"
+          value={this.state.decryptedValue}
+          onChange={this.recalculate.bind(this)}></textarea>
+      </div>
+      )
+  }
+
+  decrypt(state) {
+    var shift = state.shift;
+    var encryptedValue = state.encryptedValue;
+    var decryptedCharCodes = [];
+    for (var i = 0; i < encryptedValue.length; i++) {
+      var encryptedValueCharCode = encryptedValue.charCodeAt(i);
+      if (encryptedValueCharCode < 97 || encryptedValueCharCode > 122) {
+        decryptedCharCodes[i] = encryptedValueCharCode;
+      }
+      else {
+        var shiftedCharCode = 97 + ((26 + encryptedValueCharCode - 97 - shift) % 26);
+        decryptedCharCodes[i] = shiftedCharCode;
+      }
+    }
+    state.decryptedValue = String.fromCharCode.apply(null, decryptedCharCodes);
+  }
+
+  encrypt(state) {
+    var shift = state.shift;
+    var decryptedValue = state.decryptedValue;
+    var encryptedCharCodes = [];
+    for (var i = 0; i < decryptedValue.length; i++) {
+      var decryptedValueCharCode = decryptedValue.charCodeAt(i);
+      if (decryptedValueCharCode < 97 || decryptedValueCharCode > 122) {
+        encryptedCharCodes[i] = decryptedValueCharCode;
+      }
+      else {
+        var shiftedCharCode = 97 + ((26 + decryptedValueCharCode - 97 + shift) % 26);
+        encryptedCharCodes[i] = shiftedCharCode;
+      }
+    }
+    state.encryptedValue = String.fromCharCode.apply(null, encryptedCharCodes);
+  }
+
+  recalculate(event) {
+    event.preventDefault();
+    let formValues = this.state;
+    formValues[event.target.name] = event.target.value;
+
+    console.log(formValues);
+
+    if (event.target.name !== 'decryptedValue') {
+      this.decrypt(formValues)
+    }
+    else {
+      this.encrypt(formValues);
+    }
+
+    console.log(formValues);
+
+    this.setState(formValues);
+  }
+}
+
+export default App;
