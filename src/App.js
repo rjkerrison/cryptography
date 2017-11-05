1d57914c5786f22e54fd1476eceb098eed201442
rjkerrison <rjk1994@gmail.com>
2017-11-05 17:27:52 +0000

    Polyalphabetic cipher

--- src/App.js
+++ src/App.js
@@ -13,7 +13,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to relllllooooad.
         </p>
+        <h2>Caesar</h2>
         <Caesar/>
+        <h2>Polyalphabetic</h2>
+        <Polyalphabetic/>
       </div>
     );
   }
@@ -99,6 +102,86 @@ class Caesar extends Component {
     }
 
     console.log(formValues);
+
+    this.setState(formValues);
+  }
+}
+
+class Polyalphabetic extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      encryptedValue: 'blah blah',
+      decryptedValue: 'akzi akzi',
+      shift: 'snake'
+    }
+    this.decrypt(this.state);
+  }
+  render () {
+    return (
+      <div className="Polyalphabetic">
+        <input
+          name="shift"
+          type="text"
+          value={this.state.shift}
+          onChange={this.recalculate.bind(this)}/>
+        <textarea
+          name="encryptedValue"
+          type="text"
+          value={this.state.encryptedValue}
+          onChange={this.recalculate.bind(this)}></textarea>
+        <textarea
+          name="decryptedValue"
+          type="text"
+          value={this.state.decryptedValue}
+          onChange={this.recalculate.bind(this)}></textarea>
+      </div>
+      )
+  }
+
+  decrypt(state) {
+    state.decryptedValue = this.crypt(state.encryptedValue, state.shift, -1);
+  }
+
+  encrypt(state) {
+    state.encryptedValue = this.crypt(state.decryptedValue, state.shift, +1);
+  }
+
+  crypt(beforeValue, shift, plusOrMinus) {
+    var shiftIndex = 0;
+    var cryptCharCodes = [];
+    var shiftValues = [];
+    for (let i = 0; i < shift.length; i++) {
+      shiftValues[i] = shift.charCodeAt(i) - 96;
+    }
+
+    console.log(shiftValues);
+
+    for (let i = 0; i < beforeValue.length; i++) {
+      var cryptedCharCode = beforeValue.charCodeAt(i);
+      if (cryptedCharCode < 97 || cryptedCharCode > 122) {
+        cryptCharCodes[i] = cryptedCharCode;
+      }
+      else {
+        var shiftedCharCode = 97 + ((26 + cryptedCharCode - 97 + (plusOrMinus) * shiftValues[shiftIndex]) % 26);
+        cryptCharCodes[i] = shiftedCharCode;
+        shiftIndex = (shiftIndex + 1) % shift.length;
+      }
+    }
+    return String.fromCharCode.apply(null, cryptCharCodes);
+  }
+
+  recalculate(event) {
+    event.preventDefault();
+    let formValues = this.state;
+    formValues[event.target.name] = event.target.value.toLowerCase();
+
+    if (event.target.name === 'encryptedValue') {
+      this.decrypt(formValues)
+    }
+    else {
+      this.encrypt(formValues);
+    }
 
     this.setState(formValues);
   }
