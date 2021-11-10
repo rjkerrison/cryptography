b26cba8df1a7dc818f3332d0acdfd87037d1f052
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2021-11-10 17:58:16 +0100

    scss, rafce refactor

--- src/App.js
+++ src/App.js
@@ -1,48 +1,32 @@
-import React, { Component } from 'react'
+import { useState } from 'react'
 import Caesar from './Caesar.jsx'
 import Polyalphabetic from './Polyalphabetic.jsx'
 import Polybius from './Polybius.jsx'
-import './App.css'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      values: [
-        'firstOne',
-        'secondOne',
-        'thirdOne'
-      ]
-    }
+const App = () => {
+  const [values, setValues] = useState(['firstOne', 'secondOne', 'thirdOne'])
+
+  function updateValue(index, x) {
+    const newValues = [...values]
+    newValues.splice(index, 1, x.decryptedValue)
+
+    setValues(newValues)
   }
 
-  updateValue(index, state) {
-    var value = state.decryptedValue;
-    var values = this.state.values;
-    values[index] = value;
-    this.setState({
-      values: values
-    });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h2>Caesar ({this.state.values[0]}, {this.state.values[1]})</h2>
-        <Caesar
-          inValue={this.state.values[0]}
-          callback={(x) => this.updateValue(1, x)}
-          />
-        <h2>Polyalphabetic ({this.state.values[1]}, {this.state.values[2]})</h2>
-        <Polyalphabetic
-          inValue={this.state.values[1]}
-          callback={(x) => this.updateValue(2, x)}
-          />
-        <h2>Polybius</h2>
-        <Polybius/>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h2>
+        Caesar ({values[0]}, {values[1]})
+      </h2>
+      <Caesar inValue={values[0]} callback={(x) => updateValue(1, x)} />
+      <h2>
+        Polyalphabetic ({values[1]}, {values[2]})
+      </h2>
+      <Polyalphabetic inValue={values[1]} callback={(x) => updateValue(2, x)} />
+      <h2>Polybius</h2>
+      <Polybius />
+    </div>
+  )
 }
 
-export default App;
+export default App
